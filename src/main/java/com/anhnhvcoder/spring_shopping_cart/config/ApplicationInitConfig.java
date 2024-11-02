fd69dcafbb80ad65f84a43c962bd02eb72e93aa8
anhnhv2503 <nguyenhavietanh2503@gmail.com>
2024-11-02 10:41:37 +0700

    change jdk

--- src/main/java/com/anhnhvcoder/spring_shopping_cart/config/ApplicationInitConfig.java
+++ src/main/java/com/anhnhvcoder/spring_shopping_cart/config/ApplicationInitConfig.java
@@ -1,8 +1,10 @@
 package com.anhnhvcoder.spring_shopping_cart.config;
 
 import com.anhnhvcoder.spring_shopping_cart.exception.ResourceNotFoundException;
+import com.anhnhvcoder.spring_shopping_cart.model.Category;
 import com.anhnhvcoder.spring_shopping_cart.model.Role;
 import com.anhnhvcoder.spring_shopping_cart.model.User;
+import com.anhnhvcoder.spring_shopping_cart.repository.CategoryRepository;
 import com.anhnhvcoder.spring_shopping_cart.repository.RoleRepository;
 import com.anhnhvcoder.spring_shopping_cart.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,10 +23,44 @@ public class ApplicationInitConfig {
 
     private final PasswordEncoder passwordEncoder;
     private final RoleRepository roleRepository;
+    private final CategoryRepository categoryRepository;
 
     @Bean
     ApplicationRunner init(UserRepository userRepository){
         return args -> {
+            if(roleRepository.findAll().isEmpty()){
+                Role roleUser = new Role();
+                roleUser.setRoleName("ROLE_USER");
+                roleRepository.save(roleUser);
+                Role roleAdmin = new Role();
+                roleAdmin.setRoleName("ROLE_ADMIN");
+                roleRepository.save(roleAdmin);
+                log.warn("Default roles created: ROLE_USER, ROLE_ADMIN");
+            }
+            if(categoryRepository.findAll().isEmpty()){
+                Category c1 = new Category();
+                c1.setName("Áo thun");
+                categoryRepository.save(c1);
+                Category c2 = new Category();
+                c2.setName("Áo sơ mi");
+                categoryRepository.save(c2);
+                Category c3 = new Category();
+                c3.setName("Áo khoác");
+                categoryRepository.save(c3);
+                Category c4 = new Category();
+                c4.setName("Quần jean");
+                categoryRepository.save(c4);
+                Category c5 = new Category();
+                c5.setName("Quần kaki");
+                categoryRepository.save(c5);
+                Category c6 = new Category();
+                c6.setName("Quần âu");
+                categoryRepository.save(c6);
+                Category c7 = new Category();
+                c7.setName("Blazer");
+                categoryRepository.save(c7);
+                log.warn("Default categories created");
+            }
             if(userRepository.findByEmail("prjonlineshop@gmail.com") == null){
                 User user = new User();
                 user.setFullName("AD Văn MIN");
